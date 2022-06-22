ef4aa2b0ef2e21ef42f9a9bafcb1450d306cc30c
canofre <canofre@inf.ufsm.br>
2022-06-22 08:50:17 -0300

    Otimização

--- 2008_wscad/wscad.d
+++
@@ -1,1 +0,0 @@
-wscad.dvi wscad.d : wscad.tex  ref-wscad.bib  img/aneis.eps img/tcp_p.eps img/tcp_m.eps img/tcp_g.eps img/udp_p.eps img/udp_m.eps img/memoria.eps img/disco_taxa.eps img/disco_tmp.eps 
